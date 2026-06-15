dca7610813bbda4cd81894c4959b11134186bf08
omarhassan041 <cumarxasan04@gmail.com.>
2026-06-15 09:28:30 +0300

    waxaan update-gareeyay mashaariicda ciladaysan

--- .next/dev/server/middleware-build-manifest.js
+++ .next/dev/server/middleware-build-manifest.js
@@ -1,49 +1,11 @@
 globalThis.__BUILD_MANIFEST = {
   "pages": {
-    "/_app": [
-      "static/chunks/41f1d_next_dist_compiled_70e9835b._.js",
-      "static/chunks/41f1d_next_dist_shared_lib_3a3e7cad._.js",
-      "static/chunks/41f1d_next_dist_client_17c896d1._.js",
-      "static/chunks/41f1d_next_dist_c718eecf._.js",
-      "static/chunks/41f1d_next_app_8c5ecde7.js",
-      "static/chunks/[next]_entry_page-loader_ts_ffbab04f._.js",
-      "static/chunks/41f1d_react-dom_f99f6b50._.js",
-      "static/chunks/41f1d_579eb034._.js",
-      "static/chunks/[root-of-the-server]__876f28d4._.js",
-      "static/chunks/Projects_Chocolate-website_pages__app_2da965e7._.js",
-      "static/chunks/turbopack-Projects_Chocolate-website_pages__app_ab01d370._.js"
-    ],
-    "/_error": [
-      "static/chunks/41f1d_next_dist_compiled_70e9835b._.js",
-      "static/chunks/41f1d_next_dist_shared_lib_94384eb8._.js",
-      "static/chunks/41f1d_next_dist_client_17c896d1._.js",
-      "static/chunks/41f1d_next_dist_9fe78285._.js",
-      "static/chunks/41f1d_next_error_e188c365.js",
-      "static/chunks/[next]_entry_page-loader_ts_18bb07f1._.js",
-      "static/chunks/41f1d_react-dom_f99f6b50._.js",
-      "static/chunks/41f1d_579eb034._.js",
-      "static/chunks/[root-of-the-server]__97d3bada._.js",
-      "static/chunks/Projects_Chocolate-website_pages__error_2da965e7._.js",
-      "static/chunks/turbopack-Projects_Chocolate-website_pages__error_69edf5a2._.js"
-    ]
+    "/_app": []
   },
   "devFiles": [],
-  "polyfillFiles": [
-    "static/chunks/41f1d_next_dist_build_polyfills_polyfill-nomodule.js"
-  ],
+  "polyfillFiles": [],
   "lowPriorityFiles": [],
-  "rootMainFiles": [
-    "static/chunks/[turbopack]_browser_dev_hmr-client_hmr-client_ts_c78af447._.js",
-    "static/chunks/41f1d_next_dist_compiled_react-dom_bcfe3d8b._.js",
-    "static/chunks/41f1d_next_dist_compiled_react-server-dom-turbopack_dc5e67ef._.js",
-    "static/chunks/41f1d_next_dist_compiled_next-devtools_index_f132366b.js",
-    "static/chunks/41f1d_next_dist_compiled_2fc635d3._.js",
-    "static/chunks/41f1d_next_dist_client_ca745647._.js",
-    "static/chunks/41f1d_next_dist_54cfef41._.js",
-    "static/chunks/41f1d_@swc_helpers_cjs_ff405529._.js",
-    "static/chunks/Projects_Chocolate-website_a0ff3932._.js",
-    "static/chunks/turbopack-Projects_Chocolate-website_4260315a._.js"
-  ]
+  "rootMainFiles": []
 };
 globalThis.__BUILD_MANIFEST.lowPriorityFiles = [
 "/static/" + process.env.__NEXT_BUILD_ID + "/_buildManifest.js",
